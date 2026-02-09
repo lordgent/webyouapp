3e957d98f3bd7075183e0f78995a58d85c37a467
lordgent <Lorjenx08@gmail.com>
2026-02-09 14:37:12 +0000

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api-backend/:path*',
+        destination: 'http://203.175.11.147:8085/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
